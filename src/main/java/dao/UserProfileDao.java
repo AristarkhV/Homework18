4670aafcd3b54a98e395cc6ac7b3618080172cf1
Yulia Kiyanitsa <49162607+AristarkhV@users.noreply.github.com>
2019-07-08 17:49:49 +0300

    Update UserProfileDao.java

--- src/main/java/dao/UserProfileDao.java
+++ src/main/java/dao/UserProfileDao.java
@@ -24,19 +24,20 @@ public class UserProfileDao implements Dao<UserProfile> {
     }
 
     public Long getIdByName(String name) throws SQLException {
-        return executor.execQuery("select * from users where login='" + name + "'", resultSet -> {
+        return executor.execQuery("SELECT * FROM users WHERE login='" + name + "'", resultSet -> {
+
             resultSet.next();
             return resultSet.getLong(1);
         });
     }
 
     public void createTable() throws SQLException {
-        executor.execUpdate("create table if not exists users (id bigint auto_increment," +
+        executor.execUpdate("CREATE TABLE IF NOT EXISTS users (id bigint auto_increment," +
                 "login varchar(256), password varchar(256), primary key(id))");
     }
 
     public void insertUser(String name, String password) throws SQLException {
-        executor.execUpdate("insert into users (login, password) values " +
+        executor.execUpdate("INSERT INTO users (login, password) values " +
                 "('" + name + "'" + password + "')");
     }
 }
