ff80f37d7e0f4a56706a3785a68ec9a4115e74b6
Yulia Kiyanitsa <49162607+AristarkhV@users.noreply.github.com>
2019-07-08 18:18:01 +0300

    Update SignInServlet.java

--- src/main/java/servlets/SignInServlet.java
+++ src/main/java/servlets/SignInServlet.java
@@ -22,12 +22,18 @@ public class SignInServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        if (accountService.getUserByLogin(request.getParameter("login")) != null) {
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().write("Authorized: " + request.getParameter("login"));
-        } else {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().println("unauthorized");
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        try {
+            if (dbService.getUser(login).getPassword().equals(password)) {
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().println("Authorized: " + login);
+            } else {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().println("Unauthorized");
+            }
+        } catch (DBException e) {
+            LOGGER.error("Error in SignInServlet", e);
         }
     }
 }
